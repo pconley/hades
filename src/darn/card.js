69ab4b6db1a70b95f6be59fc3cfa1b37d485e854
pconley <pconley312@gmail.com>
2020-10-25 07:39:53 -0400

    save point

--- src/darn/card.js
+++ src/darn/card.js
@@ -2,11 +2,12 @@ import React from 'react';
 import cards from './cards';
 
 const Card = props => {
-  const width = props.size === 'small' ? 60 : 100;
+  const sizes = {small: 60, medium: 75, large: 100};
+  const width = sizes[props.size] ?? 100;
   // playing card size : 2.5in x 3.5in
   const height = 3.5 * width / 2.5;
   const src = cards[props.suit][props.value];
-  return <img src={src}  width={width} height={height} alt="Ace of Hearts" />;
+  return <img src={src} width={width} height={height} alt="" />;
 }
 
 export default Card;
--- src/pages/PlayingCardsPage.js
+++ src/pages/PlayingCardsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from "../darn/card"
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 
 import uuid from "uuid"
 import axios from "axios"
@@ -8,8 +9,9 @@ import axios from "axios"
 const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
 
 const toNumber = ({suit, value}) => {
+  const s = suits.indexOf(suit);
   const v = value === 1 ? 14 : value; // ace is big
-  return 100 * suit + v;
+  return 100 * s + v;
 }
 
 function compare(a, b) {
@@ -21,19 +23,16 @@ const contains = (cards, card) => cards.some(c => toNumber(c) === toNumber(card)
 function deal(count) {
   const rand = (a,b) => Math.floor((Math.random() * b) + a);
   const randomValue = () => rand(1, 13);
-  const randomSuit = () => rand(1, 4) - 1;
+  const randomSuit = () => suits[rand(1, 4) - 1];
   const cards = [];
-  const cardNumbers = [];
   for( let i=0; i<count; i++ ){
     let card = null;
     while (card === null || contains(cards, card)) {
-      if( card ) console.log("was dup", card);
       card = { suit: randomSuit(), value: randomValue() };
     }
     cards.push(card);
   }
-  const sorted = cards.sort(compare);
-  return sorted.map(card => ({ id: uuid.v4(), suit: suits[card.suit], value: card.value }));
+  return cards.sort(compare).map(({suit, value}) => ({ id: uuid.v4(), suit, value }));
 }
 
 class PlayingCardsPage extends React.Component {
@@ -42,7 +41,8 @@ class PlayingCardsPage extends React.Component {
     this.state = { 
       poker: deal(5),
       bigHand: deal(13),
-      remote: [],
+      remoteCards: [],
+      remoteStatus: null,
     };
   }
 
@@ -67,39 +67,55 @@ class PlayingCardsPage extends React.Component {
     .then(response => {
       console.log("hand response", response);
       const cards = response.data.hand.map(card => ({id: uuid.v4(), suit: toSuit(card.suit), value: card.rank }))
-      console.log("cards", cards);
-      this.setState({ ...this.state, remote: cards });
+      console.log("fetched cards", cards);
+      this.setState({ ...this.state, remoteCards: cards.sort(compare), remoteStatus: 'done' });
     })
-    .catch(function (error) {
+    .catch(error => {
+      let message;
       if (error.response) {
-        console.log("Request made and server responded");
-        console.log("--data", error.response.data);
-        console.log("--status", error.response.status);
-        console.log("--headers", error.response.headers);
+        message = `Request made and server responded ${error.response?.status}`;
       } else if (error.request) {
-        console.log("The request was made but no response was received");
-        console.log(error.request);
+        message = "The request was made but no response was received";
       } else {
-        console.log("Something happened in setting up the request that triggered an Error");
-        console.log('Error', error.message);
+        message = "Something happened in setting up the request that triggered an Error";
       }
-    });
+      console.warn(message);
+      console.warn(error.message);
+      console.warn("--data", error.response?.data);
+      console.warn("--status", error.response?.status);
+      console.warn("--headers", error.response?.headers);
+      this.setState({ ...this.state, remoteStatus: message });
+    })
+    this.setState({ ...this.state, remoteCards: [], remoteStatus: 'loading' });
+  }
+
+  showStatus = (status) => {
+    switch(status){
+      case null : return null;
+      case 'done' : return null;
+      case 'loading' : return <Spinner animation="border" />;
+      default: return <p>Error: {status}</p>;
+    }
   }
 
   render() {
     console.log("state", this.state);
-    const { poker, bigHand, remote } = this.state;
+    const { poker, bigHand, remoteCards } = this.state;
     return (
     <div>
       <h2 className="header">Card Images</h2>
+
       <Button onClick={() => this.dealCards()}>Deal</Button>
-      <Button onClick={() => this.getHand()}>Fetch</Button>
       <p className="pt-2">A random poker hand. Just as a place to practice image display.</p>
       {poker.map(card => <Card key={card.id} suit={card.suit} value={card.value} />)}
       <p className="pt-2">A random big hand. Just as a place to practice sizing and sorting.</p>
       {bigHand.map(card => <Card key={card.id} suit={card.suit} value={card.value} size="small" />)}
+
+      <br />
+      <Button className="mt-4" onClick={() => this.getHand()}>Fetch</Button>
       <p className="pt-2">the hand fetched from the server.</p>
-      {remote.map(card => <Card key={card.id} suit={card.suit} value={card.value} size="small" />)}
+      {remoteCards.map(card => <Card key={card.id} suit={card.suit} value={card.value} size="medium" />)}
+      {this.showStatus(this.state.remoteStatus)}
     </div>
     );
   }
