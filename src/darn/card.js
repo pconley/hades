839cbd65753811bb8545f012dd31c8f9dc8536e5
pconley <pconley312@gmail.com>
2020-10-25 17:46:58 -0400

    save point

--- src/darn/card.js
+++ src/darn/card.js
@@ -2,8 +2,14 @@ import React from 'react';
 import cards from './cards';
 
 const Card = props => {
+  const { size } = props;
   const sizes = {small: 60, medium: 75, large: 100};
-  const width = sizes[props.size] ?? 100;
+  let width = size || 100;
+  if (typeof size === 'string' || size instanceof String) {
+    if (size in sizes) {
+      width = sizes[size];
+    }
+  }
   // playing card size : 2.5in x 3.5in
   const height = 3.5 * width / 2.5;
   const src = cards[props.suit][props.value];
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,56 +1,120 @@
 import React from "react"
+
+import NumberPicker from 'react-widgets/lib/NumberPicker'
+// this import is not bringing in the expected styles????
+import 'react-widgets/dist/css/react-widgets.css';
+
 import Card from "../darn/card"
+
 import "./HomePage.scss"
 
-const Home = props => {
+class HomePage extends React.Component {
 
-  const cardWidth = 75; // medium
-  const cardHeight = 3.5 * cardWidth / 2.5; // medium
-  const width = 600;
-  const middle = width / 2;
-  const height = 400;
-  const center = height / 2 - cardHeight / 2;
-
-  const position = (n, m) => {
-    // calculate player;s position n of m at the table
-    const leftPos = (top, left) => ({ position: 'absolute', top: top + 'px', left: left + 'px'});
-    const rightPos = (top, right) => ({ position: 'absolute', top: top + 'px', right: right + 'px'});
-    switch(n) {
-      case 1: return leftPos(center, 0);
-      case m: return rightPos(center, 0);
-      default: 
-        const topPlayers = m - 2;
-        const spacing = width / (topPlayers + 1);
-        console.log("spacing", n, spacing, n*spacing);
-        return leftPos(0, spacing * (n-1) - cardWidth / 2); // FIXME
+  constructor(...args) {
+    super(...args)
+    this.state = { 
+      count: 3,
+      home: 10,
     }
-  };
+  }
 
-  const count = 5; // number of other players
-  const playerNumbers = Array.from({length: count}, (_, i) => i + 1)
+  cardWidth = 50;
+  cardHeight = 3.5 * this.cardWidth / 2.5;
 
-  return (
-    <div>
-      <h2 className="header">Table Layout Experiment</h2>
-      <div className="border table-container">
-        <div className="table-center">
-          <Card key="center" suit='spades' value="1" size="medium" />
+  tableMargin = 10;
+  tableWidth = 600;
+  tableWidthCenter = this.tableWidth / 2;
+
+  tableHeight = 2 * this.tableWidth / 3;
+  tableHeightCenter = this.tableHeight / 2;
+
+  centerWidth = 180;
+  centerHeight = 60;
+
+  handleClick = () => {
+    console.log("handle click");
+    this.setState({ ...this.state, home: this.tableHeightCenter - this.centerHeight / 2 - this.cardHeight });
+  }
+
+  moveTo = (n, m) => {
+    console.log("move to", n, m);
+  }
+
+  render() {
+
+    const { count, home } = this.state;
+
+    const position = (n, m) => {
+      // calculate the position of player N of M at the table
+      const leftPos = (top, left) => ({ position: 'absolute', top: top + 'px', left: left + 'px'});
+      const rightPos = (top, right) => ({ position: 'absolute', top: top + 'px', right: right + 'px'});
+      switch(n) {
+        case 1: return leftPos(this.tableHeightCenter - this.cardHeight / 2, this.tableMargin);
+        case m: return rightPos(this.tableHeightCenter - this.cardHeight / 2, this.tableMargin);
+        default: 
+          const topPlayers = m - 2;
+          const width = this.tableWidth - 2 * this.tableMargin;
+          const spacing = width / (topPlayers + 1);
+          return leftPos(this.tableMargin, this.tableMargin + spacing * (n-1) - this.cardWidth / 2);
+      }
+    };
+
+    let playerNumbers = Array.from({length: count}, (_, i) => i + 1);
+
+    let homePosition = { bottom: home + 'px' };
+
+    let centerPos = {
+      position: "absolute",
+      top: this.tableHeightCenter - this.centerHeight / 2,
+      left: this.tableWidthCenter - this.centerWidth / 2,
+      width: this.centerWidth,
+      height: this.centerHeight,
+      border: "1px solid #73AD21",
+    }
+
+    return (
+      <div>
+        <h2 className="header">Table Layout Experiment</h2>
+        <p>The cards are just placholders to explore positioning stuff at the edges and center of the green table.</p>
+        Select 3 to 7 Other Players : <NumberPicker
+          max={7} min={3}
+          value={count}
+          onChange={value => this.setState({ count: value })}
+        />
+        <div className="border table-container" style={{width: this.tableWidth, height: this.tableHeight}}>
+
+          <div style={centerPos}>center</div>
+
+          <div key="me" className="table-home" 
+            style={homePosition}
+            onClick={this.handleClick}
+          >
+            <Card suit='hearts' value="13" size="small" />
+          </div>
+
+          {playerNumbers.map(n => {
+            return (
+              <div key={n+2} 
+                style={position(n, count)} 
+                onClick={() => this.moveTo(n, count)}
+              >
+                <Card suit='clubs' value={n+2} size={this.cardWidth} />
+              </div>
+            )
+          })}
+
+          <div className="content left-content">
+            There are {count} players.
+          </div>
+
+          <div className="content right-content">
+            some right content
+          </div>
+
         </div>
-
-        <div key="me" className="table-home">
-          <Card suit='hearts' value="13" size="medium" />
-        </div>
-
-        {playerNumbers.map(n => {
-          return (
-            <div key={n+2} style={position(n, count)}>
-              <Card suit='clubs' value={n+2} size="medium" />
-            </div>
-          )
-        })}
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default Home
+export default HomePage
